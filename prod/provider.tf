04485629d183f663c9f0cc40c6f55134be78dddd
pedrocristovao <pedroluiz.cristovao@gmail.com>
2025-07-13 17:43:43 +0200

    fix provider on prod + refactor CICD

--- prod/provider.tf
+++ prod/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+    required_version = ">= 1.5.0"
+    required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "~> 4.36"
+        }
+    }
+}
+
 provider "azurerm" {
     features {}
 }

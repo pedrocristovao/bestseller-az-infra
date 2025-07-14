e200ead40082aad964a09f24c300293dde2fbd5c
pedrocristovao <pedroluiz.cristovao@gmail.com>
2025-07-14 13:27:17 +0200

    restarting tfstate

--- prod/main.tf
+++ prod/main.tf
@@ -1,40 +1,40 @@
-locals {
-    environment     = "prod"
-    location    = "West Europe"
-}
+# locals {
+#     environment     = "prod"
+#     location    = "West Europe"
+# }
 
-module "rg" {
-    source  = "../modules/resource_group"
+# module "rg" {
+#     source  = "../modules/resource_group"
 
-    name    = "rg-hello-${local.environment}"
-    location = local.location
-}
+#     name    = "rg-hello-${local.environment}"
+#     location = local.location
+# }
 
-module "storage" {
-    source              = "../modules/storage_account"
+# module "storage" {
+#     source              = "../modules/storage_account"
 
-    name                = "storagehello${local.environment}"
-    resource_group_name = module.rg.name
-    location            = module.rg.location
-}
+#     name                = "storagehello${local.environment}"
+#     resource_group_name = module.rg.name
+#     location            = module.rg.location
+# }
 
-module "app_plan" {
-    source              = "../modules/app_service_plan"
+# module "app_plan" {
+#     source              = "../modules/app_service_plan"
 
-    name                = "plan-hello-${local.environment}"
-    location            = local.location
-    resource_group_name = module.rg.name
-    os_type            = "Linux"
-    sku_name           = "Y1"
-}
+#     name                = "plan-hello-${local.environment}"
+#     location            = local.location
+#     resource_group_name = module.rg.name
+#     os_type            = "Linux"
+#     sku_name           = "Y1"
+# }
 
-module "function_app" {
-    source                      = "../modules/function_app"
+# module "function_app" {
+#     source                      = "../modules/function_app"
 
-    name                        = "func-hello-${local.environment}"
-    resource_group_name         = module.rg.name
-    location                    = module.rg.location
-    service_plan_id             = module.app_plan.id
-    storage_account_name        = module.storage.name
-    storage_account_access_key  = module.storage.primary_access_key
-}
+#     name                        = "func-hello-${local.environment}"
+#     resource_group_name         = module.rg.name
+#     location                    = module.rg.location
+#     service_plan_id             = module.app_plan.id
+#     storage_account_name        = module.storage.name
+#     storage_account_access_key  = module.storage.primary_access_key
+# }

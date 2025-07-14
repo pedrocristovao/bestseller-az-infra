211c15f187b392f1cbcd89ecb67a6a0f57cfbfcc
pedrocristovao <pedroluiz.cristovao@gmail.com>
2025-07-14 12:54:34 +0200

    remove backend.tf

--- triage/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "rg-tfstate-backend_setup"
-    storage_account_name  = "storagetfstateinfra"
-    container_name        = "tfstate"
-    key                   = "triage.terraform.tfstate"
-    use_azuread_auth = true
-  }
-}
